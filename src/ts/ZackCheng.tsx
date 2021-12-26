54b73a977741896b7904535d91d34ce5c39297ae
Zack Cheng <zack_cheng@brown.edu>
2021-12-26 17:49:30 -0500

    improved animation

--- src/ts/ZackCheng.tsx
+++ src/ts/ZackCheng.tsx
@@ -1,33 +1,71 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../css/ZackCheng.scss'
 import { PixelLetter, Z, A, C, K, space, H, E, N, G } from './PixelLetter.type'
 import useWindowDimensions from './useWindowDimensions'
 
-const starPixMargin = 0
+const pixSize = 5
+const starPixSizeModifier = 0.6
+const starPixMargin = 5
 
 const timing = {
-  overallDuration: 4,
-  getDelay: () => Math.max(Math.random(), Math.random()),
+  overallDuration: 2.5,
+  getDelay: () => {
+    if (Math.random() < 0.1) {
+      return 0
+    }
+    const roll1 = Math.random()
+    const roll2 = Math.random()
+    const roll3 = Math.random()
+    return (2 / 3) * (roll1 + roll2 + roll3 - Math.min(roll1, roll2, roll3))
+  },
 }
 
-function Pixel({ xy, svgDimensions }: { xy: number[]; svgDimensions: number[] }) {
-  const [x, y] = xy
-  let [pixHeight, pixWidth] = svgDimensions
-  pixHeight -= 2 * starPixMargin
-  pixWidth -= 2 * starPixMargin
+const colors = [
+  '#f0e5bb', // yellow
+  '#fcedb3', // yellow
+  '#fabb93', // orange
+  '#d16a45', // orange-red
+  '#cf5a4a', // red
+  '#5992e3', // blue
+  '#c7deff', // white-blue
+  '#f0bbde', // pink
+]
+function selectRandomColor() {
+  if (Math.random() < 0.6) {
+    return 'white'
+  }
+  return colors[(Math.random() * colors.length) >> 0]
+}
 
-  const starPosX = Math.floor(Math.random() * pixWidth) + starPixMargin,
-    starPosY = Math.floor(Math.random() * pixHeight) + starPixMargin
-  const starOffsetX = starPosX - x,
-    starOffsetY = starPosY - y
+function getRandomStarPos(h: number, w: number) {
+  return [
+    ((Math.random() - 0.5) / starPixSizeModifier + 0.5) * w + starPixMargin,
+    ((Math.random() - 0.5) / starPixSizeModifier + 0.5) * h + starPixMargin,
+  ]
+}
+
+// Components
+
+type XYDimensions = { x: number; y: number }
+
+function Pixel({ xy, pixDimensions }: { xy: number[]; pixDimensions: XYDimensions }) {
+  const [x, y] = xy
+  let { x: pixWidth, y: pixHeight } = pixDimensions
+  pixHeight -= 2 * starPixMargin + 1
+  pixWidth -= 2 * starPixMargin + 1
+
+  const [starPosX, starPosY] = getRandomStarPos(pixHeight, pixWidth)
+  const starOffsetX = (starPosX - x) * starPixSizeModifier,
+    starOffsetY = (starPosY - y) * starPixSizeModifier
 
   const delay = timing.getDelay()
   const transitionTime = timing.overallDuration - delay
-  // setTimeout(() => setIsStar(false), delay * 1000)
 
   const style = {
-    transition: `transform ${transitionTime}s cubic-bezier(0.7, 0, 0.3, 1) ${delay}s`,
-    '--star-transform': `translate(${starOffsetX}px, ${starOffsetY}px)`,
+    '--transition-reduced-motion': `0s ${delay}s`,
+    '--transition': `${transitionTime}s cubic-bezier(0.7, 0, 0.3, 1) ${delay}s`,
+    '--fill': selectRandomColor(),
+    '--transform': `translate(${starOffsetX}px, ${starOffsetY}px) scale(${starPixSizeModifier})`,
   } as React.CSSProperties
 
   return <rect id={`${x},${y}`} className="pixel" width="1" height="1" x={x} y={y} style={style} />
@@ -36,35 +74,38 @@ function Pixel({ xy, svgDimensions }: { xy: number[]; svgDimensions: number[] })
 function PixelLetterSVGFragment({
   xyOffset,
   data,
-  svgDimensions,
+  pixDimensions,
 }: {
   xyOffset: number[]
   data: PixelLetter['data']
-  svgDimensions: number[]
+  pixDimensions: XYDimensions
 }) {
   const [xOff, yOff] = xyOffset
   return (
     <>
       {data.map(({ x, y }, i) => (
-        <Pixel key={i} xy={[x + xOff, y + yOff]} svgDimensions={svgDimensions} />
+        <Pixel key={i} xy={[x + xOff, y + yOff]} pixDimensions={pixDimensions} />
       ))}
     </>
   )
 }
 
 function ZackCheng() {
-  const pixelSize = 4
   const letterArray: PixelLetter[] = [Z, A, C, K, space, C, H, E, N, G]
+  const textX = letterArray.reduce((acc, curr) => (acc += curr.width), 0) + letterArray.length - 1
+  const textY = 5
+
   const { height, width } = useWindowDimensions()
-  const [className, setClassName] = useState('')
+  const [className, setClassName] = useState('stars')
 
-  const pixHeight = height / pixelSize,
-    pixWidth = width / pixelSize
+  const pixDimensions: XYDimensions =
+    className === 'text' ? { x: textX, y: textY } : { x: width / pixSize, y: height / pixSize }
+  const style: React.CSSProperties =
+    className === 'text' ? { height: textY * pixSize, width: textX * pixSize } : { height, width }
+  const otherStyle = { '--transition': 'transform 1.75s cubic-bezier(0.7, 0, 0.3, 1)' } as React.CSSProperties
 
-  const textXWidth = letterArray.reduce((acc, curr) => (acc += curr.width), 0) + letterArray.length - 1
-  console.log(textXWidth)
-  const baseYOffset = pixHeight / 2 - 2.5,
-    baseXOffset = pixWidth / 2 - textXWidth / 2
+  const baseXOffset = pixDimensions.x / 2 - textX / 2
+  const baseYOffset = pixDimensions.y / 2 - textY / 2
 
   const svgFragment: any[] = []
   let xOffsetAccumulator = 0
@@ -75,21 +116,28 @@ function ZackCheng() {
         key={i}
         xyOffset={[xOffsetAccumulator + baseXOffset, baseYOffset]}
         data={letter.data}
-        svgDimensions={[pixHeight, pixWidth]}
+        pixDimensions={pixDimensions}
       />
     )
     xOffsetAccumulator += letter.width + 1
   }
 
-  setTimeout(() => setClassName('start-animation'), 0) // todo
+  function onClick() {
+    setClassName('coalesce')
+    setTimeout(() => {
+      setClassName('text')
+    }, timing.overallDuration * 1000)
+  }
 
   return (
     <div id="ZackCheng">
       <svg
         id="ZackCheng-svg"
         className={className}
+        onClick={className === 'stars' ? onClick : undefined}
         xmlns="http://www.w3.org/2000/svg"
-        viewBox={`0 0 ${pixWidth} ${pixHeight}`}>
+        viewBox={`0 0 ${pixDimensions.x} ${pixDimensions.y}`}
+        style={{ ...style, ...otherStyle }}>
         {svgFragment}
       </svg>
     </div>
