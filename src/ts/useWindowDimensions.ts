23960b3b1351a9e02fea102f8058f8b51ca5f15e
Zack Cheng <zack_cheng@brown.edu>
2021-12-26 18:17:13 -0500

    debounced window resize hook

--- src/ts/useWindowDimensions.ts
+++ src/ts/useWindowDimensions.ts
@@ -1,20 +1,34 @@
 import { useState, useEffect } from 'react'
 
+const debounceDelay = 100
+
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window
-  return { width, height }
+  return { height: window.innerHeight, width: window.innerWidth }
 }
 
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions)
 
   useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions())
+    let isWaiting: number = 0
+
+    const debouncedHandleResize = () => {
+      if (isWaiting) {
+        return
+      }
+
+      isWaiting = setTimeout(() => {
+        isWaiting = 0
+        const hw = getWindowDimensions()
+        if (hw.height === windowDimensions.height && hw.width === windowDimensions.width) {
+          return
+        }
+        setWindowDimensions(hw)
+      }, debounceDelay)
     }
 
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
+    window.addEventListener('resize', debouncedHandleResize)
+    return () => window.removeEventListener('resize', debouncedHandleResize)
   }, [])
 
   return windowDimensions
