5a38a170f700a4c404494875ae081278ccab9f57
Zack Cheng <zack_cheng@brown.edu>
2021-12-27 15:05:28 -0500

    modified animstar dist

--- src/ts/svgPixels/PixelStarField.tsx
+++ src/ts/svgPixels/PixelStarField.tsx
@@ -3,6 +3,8 @@ import { XY } from './types'
 import { PixelAnimStarProps, PixelAnimStar } from './PixelAnimStar'
 import { PixelStar, PixelStarProps } from './PixelStar'
 
+// Helper functions
+
 const colors = [
   '#f0e5bb', // yellow
   '#fcedb3', // yellow
@@ -13,12 +15,28 @@ const colors = [
   '#c7deff', // white-blue
   '#f0bbde', // pink
 ]
-function selectRandomColor() {
+function selectRandomColor(): string {
   const roll = Math.random()
   if (roll > 0.5) {
     return '#ffffff'
   }
   return colors[(roll * 2 * colors.length) >> 0]
+}
+
+function getAnimStarPosition(pixelDimensions: XY): XY {
+  // Modify distribution to favour edges of screen using quad ease-in-out
+  function randomFunction(): number {
+    const x = Math.random()
+    if (x < 0.5) {
+      return 2 * x * x
+    }
+    return 1 - Math.pow(-2 * x + 2, 2) / 2
+  }
+
+  return {
+    x: Math.round(randomFunction() * pixelDimensions.x),
+    y: Math.round(randomFunction() * pixelDimensions.y),
+  }
 }
 
 type PixelStarFieldProps = {
@@ -36,10 +54,7 @@ export function PixelStarField({ pixelDimensions }: PixelStarFieldProps) {
     const { x: xDim, y: yDim } = pixelDimensions
 
     const animStarsTemp: PixelAnimStarProps[] = [...Array(numAnimStars).keys()].map(() => ({
-      position: {
-        x: Math.round(Math.random() * xDim),
-        y: Math.round(Math.random() * yDim),
-      },
+      position: getAnimStarPosition(pixelDimensions),
       color: selectRandomColor(),
       scale: { x: 1, y: 1 },
       offsetFrac: Math.random(),
