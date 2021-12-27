d5e60a8a37b61390bd3e71f80f817789a1b20422
Zack Cheng <zack_cheng@brown.edu>
2021-12-26 19:28:12 -0500

    fix for vh bug on ios Safari

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -5,8 +5,13 @@ import ZackCheng from './ZackCheng'
 function App() {
   const hwDimensions = useWindowDimensions()
 
+  const style = {
+    '--viewport-height': hwDimensions.height + 'px',
+    '--viewport-width': hwDimensions.width + 'px',
+  } as React.CSSProperties
+
   return (
-    <div className="App">
+    <div className="App" style={style}>
       <ZackCheng hwDimensions={hwDimensions} />
     </div>
   )
