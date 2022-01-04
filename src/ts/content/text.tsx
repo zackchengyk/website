e269444575279ba4cf54bb0dfc381172ca221789
Zack Cheng <zack_cheng@brown.edu>
2022-01-04 04:14:43 -0500

    laid out images and text for projects

--- src/ts/content/text.tsx
+++ src/ts/content/text.tsx
@@ -1,9 +1,8 @@
-import brgdSrc from '../../img/experience/brgd-logo-small.png'
-import brownSrc from '../../img/experience/brown-logo.svg'
-import govtechSrc from '../../img/experience/govtech-logo-purple-small.png'
+import { ExperienceName } from '../sections/ExperienceSection'
+import { ExperienceListItemContentProps } from '../sections/ExperienceListItem'
 import OutLink from './OutLink'
-import { ExperienceListItemContentProps, ExperienceName } from '../sections/ExperienceSection'
-import { ProjectName, ProjectsListItemContentProps } from '../sections/ProjectsSection'
+import { ProjectName } from '../sections/ProjectsSection'
+import { ProjectsListItemContentProps } from '../sections/ProjectsListItem'
 
 // ================== About
 
@@ -47,6 +46,10 @@ export const personalBlurb = (
 )
 
 // ================== Experience
+
+import brgdSrc from '../../img/experience/brgd-logo-small.png'
+import brownSrc from '../../img/experience/brown-logo.svg'
+import govtechSrc from '../../img/experience/govtech-logo-purple-small.png'
 
 export const experienceText: Record<ExperienceName, ExperienceListItemContentProps> = {
   govtech: {
@@ -119,29 +122,242 @@ export const experienceText: Record<ExperienceName, ExperienceListItemContentPro
 
 // ================== Projects
 
+import cityscape from '../../img/projects/cityscape'
+import maps from '../../img/projects/maps'
+import voxels from '../../img/projects/voxels'
+import override from '../../img/projects/override'
+import spacewar from '../../img/projects/spacewar'
+
+const embeddedVideoCommon = {
+  preload: 'metadata',
+  loop: true,
+  muted: true,
+  playsInline: true,
+  controls: true,
+}
+const pseudoGifCommon = {
+  className: 'pseudo-gif',
+  autoPlay: true,
+  loop: true,
+  muted: true,
+  playsInline: true,
+  controls: false,
+}
+
 export const projectsText: Record<ProjectName, ProjectsListItemContentProps> = {
   website: {
     projectName: 'Website',
-    children: <>{'Hello'}</>,
+    imgChildren: <img src={spacewar.imageSrc} alt="todo: Spacewar image" />,
+    projectTitle: "I'm Talking 'Bout This Website (You're Looking At It)",
+    projectSubtitle: 'React, Typescript, THREE.Js',
+    projectBody: <p>{'My God this was tiring.'}</p>,
   },
   cityscape: {
     projectName: 'Cityscape',
-    children: <>{'Hello'}</>,
+    imgChildren: (
+      <div style={{ display: 'grid', gridGap: 10, gridTemplateAreas: '"a a a" "b c d"' }}>
+        <video {...embeddedVideoCommon} poster={cityscape.recordingPosterSrc} style={{ gridArea: 'a' }}>
+          <source src={cityscape.recordingWebmSrc} type="video/webm" />
+          <source src={cityscape.recordingMp4Src} type="video/mp4" />
+        </video>
+        <img src={cityscape.image3Src} alt="todo" />
+        <img src={cityscape.image2Src} alt="todo" />
+        <img src={cityscape.image1Src} alt="todo" />
+      </div>
+    ),
+    projectBody: <p>{'Cityscape!'}</p>,
   },
   maps: {
     projectName: 'Maps',
-    children: <>{'Hello'}</>,
+    imgChildren: (
+      <div style={{ display: 'grid', gridGap: 10, gridTemplateAreas: '"a a a" "b c d"' }}>
+        <img src={maps.imageSrc} alt="todo" style={{ gridArea: 'a' }} />
+        <video {...pseudoGifCommon}>
+          <source src={maps.recording1WebmSrc} type="video/webm" />
+          <source src={maps.recording1Mp4Src} type="video/mp4" />
+        </video>
+        <video {...pseudoGifCommon}>
+          <source src={maps.recording2WebmSrc} type="video/webm" />
+          <source src={maps.recording2Mp4Src} type="video/mp4" />
+        </video>
+        <video {...pseudoGifCommon}>
+          <source src={maps.recording3WebmSrc} type="video/webm" />
+          <source src={maps.recording3Mp4Src} type="video/mp4" />
+        </video>
+      </div>
+    ),
+    projectBody: (
+      <>
+        <p>
+          {'I made a '}
+          <strong>{'browser-based'}</strong>
+          {', IntelliJ-themed map applet in React, with a Java backend.'}
+        </p>
+        <p>
+          {'The app features pathfinding via '}
+          <strong>
+            <OutLink href="https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm">
+              {"Dijkstra's algorithm"}
+            </OutLink>
+          </strong>
+          {', nearest-node searching using '}
+          <strong>
+            <abbr title="k-dimensional">{'k-d'}</abbr> {'trees'}
+          </strong>
+          {', and '}
+          <strong>{'server- and client-side caching'}</strong>
+          {'. Its UI is also fully '}
+          <strong>{'keyboard-navigable'}</strong>
+          {' and '}
+          <strong>{'responsive'}</strong>.
+        </p>
+      </>
+    ),
   },
   voxels: {
     projectName: 'Voxel Reconstruction',
-    children: <>{'Hello'}</>,
+    imgChildren: (
+      <div style={{ display: 'grid', gridGap: 10, gridTemplateAreas: '"a b c d" "e f g h"' }}>
+        <img src={voxels.imageBirdSrc} alt="todo" />
+        <video {...pseudoGifCommon}>
+          <source src={voxels.recordingBird10WebmSrc} type="video/webm" />
+          <source src={voxels.recordingBird10Mp4Src} type="video/mp4" />
+        </video>
+        <video {...pseudoGifCommon}>
+          <source src={voxels.recordingBird20WebmSrc} type="video/webm" />
+          <source src={voxels.recordingBird20Mp4Src} type="video/mp4" />
+        </video>
+        <video {...pseudoGifCommon}>
+          <source src={voxels.recordingBird100WebmSrc} type="video/webm" />
+          <source src={voxels.recordingBird100Mp4Src} type="video/mp4" />
+        </video>
+        <img src={voxels.imagePigSrc} alt="todo" />
+        <video {...pseudoGifCommon}>
+          <source src={voxels.recordingPig10WebmSrc} type="video/webm" />
+          <source src={voxels.recordingPig10Mp4Src} type="video/mp4" />
+        </video>
+        <video {...pseudoGifCommon}>
+          <source src={voxels.recordingPig20WebmSrc} type="video/webm" />
+          <source src={voxels.recordingPig20Mp4Src} type="video/mp4" />
+        </video>
+        <video {...pseudoGifCommon}>
+          <source src={voxels.recordingPig180WebmSrc} type="video/webm" />
+          <source src={voxels.recordingPig180Mp4Src} type="video/mp4" />
+        </video>
+      </div>
+    ),
+    projectBody: (
+      <>
+        <p>
+          {'We implemented '}
+          <strong>{'photorealistic scene reconstruction'}</strong>
+          {' via '}
+          <strong>{'voxel coloring'}</strong>
+          {', and extended it to produce 3D-printable '}
+          <strong>{'.obj files'}</strong>
+          {' based on '}
+          <strong>{'images'}</strong>
+          {' taken from multiple vantage points. Our three-person team was inspired by '}
+          <OutLink href="https://doi.org/10.1023/A:1008176507526">{'this paper'}</OutLink>
+          {', and we built our app using '}
+          <strong>{'Python'}</strong>
+          {' and '}
+          <strong>{'MATLAB'}</strong>.
+        </p>
+      </>
+    ),
   },
   override: {
     projectName: 'Override',
-    children: <>{'Hello'}</>,
+    imgChildren: (
+      <div style={{ display: 'grid', gridGap: 10, gridTemplateAreas: '"a a a" "b c d"' }}>
+        <video {...pseudoGifCommon} style={{ gridArea: 'a' }}>
+          <source src={override.recording1WebmSrc} type="video/webm" />
+          <source src={override.recording1Mp4Src} type="video/mp4" />
+        </video>
+        <video {...pseudoGifCommon}>
+          <source src={override.recording2WebmSrc} type="video/webm" />
+          <source src={override.recording2Mp4Src} type="video/mp4" />
+        </video>
+        <video {...pseudoGifCommon}>
+          <source src={override.recording3WebmSrc} type="video/webm" />
+          <source src={override.recording3Mp4Src} type="video/mp4" />
+        </video>
+        <video {...pseudoGifCommon}>
+          <source src={override.recording4WebmSrc} type="video/webm" />
+          <source src={override.recording4Mp4Src} type="video/mp4" />
+        </video>
+      </div>
+    ),
+    projectBody: (
+      <>
+        <p>
+          {'With some very talented teammates from the '}
+          <OutLink href="https://twitter.com/brownrisdgames">
+            {'Brown-'}
+            <abbr title="Rhode Island School of Design">{'RISD'}</abbr>
+            {' Game Developers club'}
+          </OutLink>
+          {', we made an arcade-style shoot-em-up where you take control of your enemies: '}
+          <OutLink href="https://rainydey.itch.io/override">{'Override! (available on Itch.io)'}</OutLink>
+        </p>
+        <p>
+          {
+            'I worked on game design, movement, particle effects, and code organization; mostly, this was a chance to brush up on my '
+          }
+          <strong>{'Unity'}</strong>
+          {' and '}
+          <strong>{'C#'}</strong>
+          {', plus some UI design and real-time graphics fundamentals.'}
+        </p>
+      </>
+    ),
   },
   spacewar: {
     projectName: 'Spacewar!',
-    children: <>{'Hello'}</>,
+    imgChildren: <img src={spacewar.imageSrc} alt="todo" />,
+    projectTitle: 'Spacewar!',
+    projectSubtitle: 'HTML5 Canvas',
+    projectBody: (
+      <>
+        <p>
+          {'Alan Kay once noted: "the game of '}
+          <cite>
+            <OutLink href="https://en.wikipedia.org/wiki/Spacewar!">{'Spacewar!'}</OutLink>
+          </cite>
+          {
+            ' blossoms spontaneously wherever there is a graphics display connected to a computer". So, here\'s '
+          }
+          <OutLink href="https://zackchengyk.github.io/spacewar/">
+            <em>{'my'}</em>
+            {' version'}
+          </OutLink>
+          {' of the original 1962 game :)'}
+        </p>
+        <p>
+          {'I kept the project files simple so that non-'}
+          <abbr title="computer science">{'CS'}</abbr>
+          {' art students could not only '}
+          <strong>
+            <em>{'play'}</em>
+            {' the game'}
+          </strong>
+          {', but also '}
+          <strong>
+            <em>{'play with its source code'}</em>
+          </strong>
+          {
+            '. Through their involvement as viewers in the creation of the experience of interacting with the work, I hoped to help them learn about '
+          }
+          <OutLink href="https://en.wikipedia.org/wiki/Early_history_of_video_games">
+            {'early video games'}
+          </OutLink>
+          {' as an offshoot of interactive media/art.'}
+        </p>
+        <p>
+          <OutLink href="https://zackchengyk.github.io/spacewar/">{'> Play it here!'}</OutLink>
+        </p>
+      </>
+    ),
   },
 }
