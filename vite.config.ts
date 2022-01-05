2f5c421ef216fd83b63d8fc2fb2ed69d068a12e2
Zack Cheng <zack_cheng@brown.edu>
2022-01-05 06:19:43 -0500

    modified build directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'zackchengyk.github.io',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
