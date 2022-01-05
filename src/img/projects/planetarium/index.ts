6549de7691497632a7d1d4cdd5ff870e8d8bb4a1
Zack Cheng <zack_cheng@brown.edu>
2022-01-05 04:49:31 -0500

    added planetarium images

---
+++ src/img/projects/planetarium/index.ts
@@ -0,0 +1,19 @@
+import image1SmallSrc from './planetarium-image-1-small.png'
+import image1MediumSrc from './planetarium-image-1-medium.png'
+import image2SmallSrc from './planetarium-image-2-small.png'
+import image2MediumSrc from './planetarium-image-2-medium.png'
+import image3SmallSrc from './planetarium-image-3-small.png'
+import image3MediumSrc from './planetarium-image-3-medium.png'
+import image4SmallSrc from './planetarium-image-4-small.png'
+import image4MediumSrc from './planetarium-image-4-medium.png'
+
+export default {
+  image1SmallSrc,
+  image1MediumSrc,
+  image2SmallSrc,
+  image2MediumSrc,
+  image3SmallSrc,
+  image3MediumSrc,
+  image4SmallSrc,
+  image4MediumSrc,
+}
