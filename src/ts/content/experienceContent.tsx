7fcc7451ea55ff26a02ccea402fe8063d454d24d
Zack Cheng <zack_cheng@brown.edu>
2022-01-06 06:33:18 -0500

    experience content

--- src/ts/content/experienceContent.tsx
+++ src/ts/content/experienceContent.tsx
@@ -12,14 +12,33 @@ export type ExperienceName = typeof experienceNames[number]
 const govtech: ExperienceListItemContentProps = {
   experienceName: 'Govtech',
   imgSrc: govtechSrc,
-  experienceTitle: 'Frontend Software Developer',
+  experienceTitle: 'Frontend Developer',
   experienceSubtitle: 'Government Technology Agency',
   experienceBody: (
-    <p>
-      {
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.'
-      }
-    </p>
+    <>
+      <p>
+        {'While contributing to the development of '}
+        <OutLink href="https://supportgowhere.life.gov.sg">
+          <strong>{'SupportGoWhere'}</strong>
+        </OutLink>
+        {', I built a library of '}
+        <strong>{'React'}</strong>
+        {' components for user '}
+        <strong>{'input/interaction'}</strong>
+        {' and initiated '}
+        <strong>
+          <abbr title="accessibility">{'a11y'}</abbr>
+        </strong>
+        {" efforts as the team's primary "}
+        <strong>{'accessibility reviewer'}</strong>
+        {'.'}
+      </p>
+      <p>
+        {'I also designed and developed a '}
+        <strong>{'data pipeline + CMS'}</strong>
+        {' to manage dynamic content provided by external partners (in multiple languages!).'}
+      </p>
+    </>
   ),
 }
 
@@ -32,9 +51,10 @@ const brgd: ExperienceListItemContentProps = {
   experienceSubtitle: 'Brown-RISD Game Developers',
   experienceBody: (
     <p>
-      {
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.'
-      }
+      {'Right now, I just help to run the club and oversee game design and development. '}
+      {"In the near future, I'll also be working on improving our "}
+      <strong>{'Unity and C# instruction'}</strong>
+      {' ;)'}
     </p>
   ),
 }
@@ -45,16 +65,20 @@ const csci1430: ExperienceListItemContentProps = {
   experienceName: 'CSCI 1430',
   imgSrc: brownSrc,
   experienceTitle: 'Head Teaching Assistant',
-  experienceSubtitle: 'CSCI 1430: Computer Vision',
+  experienceSubtitle: 'CSCI 1430: Computer Vision, Spring 2022',
   experienceBody: (
     <p>
-      {"I'm one of two "}
+      {"I'm currently one of two "}
       <abbr title="head teaching assistants">{'HTAs'}</abbr>
       {' for '}
       <OutLink href="https://browncsci1430.github.io/webpage/index.html">
         {'CSCI 1430 (Computer Vision)'}
       </OutLink>
-      {', Spring 2022.'}
+      {". We're still in the midst of preparations, including reviewing "}
+      <strong>{'course content'}</strong>
+      {' and designing + producing '}
+      <strong>{'interactive demos'}</strong>
+      {'. Check back later!'}
     </p>
   ),
 }
@@ -65,13 +89,18 @@ const engn0031: ExperienceListItemContentProps = {
   experienceName: 'ENGN 0031',
   imgSrc: brownSrc,
   experienceTitle: 'Undergraduate Teaching Assistant',
-  experienceSubtitle: 'ENGN 0031: Honors Intro to Engineering',
+  experienceSubtitle: 'ENGN 0031: Honors Intro to Engineering, Spring 2021',
   experienceBody: (
-    <p>
-      {'I was one of four '}
-      <abbr title="undergraduate teaching assistants">{'UTAs'}</abbr>
-      {' for ENGN 0031 (Honors Intro to Engineering), Spring 2021.'}
-    </p>
+    <>
+      <p>
+        {'I guided students through problem sets and coding in '}
+        <strong>{'MATLAB'}</strong>
+        {' and '}
+        <strong>{'Mathematica'}</strong>
+        {', almost always extending my hours >2x to help those who stuck around. '}
+        {' I loved every minute of my time as a UTA, and hope it helped!'}
+      </p>
+    </>
   ),
 }
 
