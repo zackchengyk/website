82edabcdf46477566f5a3f7b421d87e030d3ae8c
Zack Cheng <zack_cheng@brown.edu>
2022-01-09 04:47:23 -0500

    modified windowDimensions hook to use a special throttle instead of debounce

--- src/ts/useWindowDimensions.ts
+++ src/ts/useWindowDimensions.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { XY } from './common'
 
-const debounceDelay = 50
+const terminateIntervalDelay = 50
 
 function getWindowDimensions(): XY {
   const hw = { x: document.documentElement.clientWidth, y: document.documentElement.clientHeight }
@@ -12,21 +12,37 @@ export function useWindowDimensions(): XY {
   const [windowDimensions, setWindowDimensions] = useState<XY>(getWindowDimensions)
 
   useEffect(() => {
+    let nextFrameReq = 0
     let handle = 0
-    function resizeHandler() {
-      // Debounce
-      clearTimeout(handle)
-      handle = setTimeout(() => {
-        // Set window dimensions
-        const hw = getWindowDimensions()
-        if (hw.x === windowDimensions.x && hw.y === windowDimensions.y) {
-          return
-        }
-        setWindowDimensions(hw)
-      }, debounceDelay)
+
+    // Function to update windowDimensions with animation frames
+    function updateSizeViaRAF() {
+      nextFrameReq = requestAnimationFrame(updateSizeViaRAF)
+
+      // Update windowDimensions if it changed
+      const hw = getWindowDimensions()
+      if (hw.x === windowDimensions.x && hw.y === windowDimensions.y) {
+        return
+      }
+      setWindowDimensions(hw)
     }
 
-    window.addEventListener('resize', resizeHandler)
+    function resizeHandler() {
+      // Start throttled windowDimensions-updating if not already started
+      if (!nextFrameReq) {
+        nextFrameReq = requestAnimationFrame(updateSizeViaRAF)
+      }
+
+      // Terminate throttled windowDimensions-updating after some time of no resizing
+      clearTimeout(handle)
+      handle = setTimeout(
+        () => (cancelAnimationFrame(nextFrameReq), (nextFrameReq = 0)),
+        terminateIntervalDelay
+      )
+    }
+
+    // Add event listener
+    window.addEventListener('resize', resizeHandler, { passive: true })
     return () => window.removeEventListener('resize', resizeHandler)
   }, [])
 
