90a88aad39fcaa18483816d06354fce804599031
Zack Cheng <zack_cheng@brown.edu>
2022-01-10 04:48:33 -0500

    uploaded art files

---
+++ src/img/art/index.ts
@@ -0,0 +1,39 @@
+import architectureSrc from './architecture-small.jpg'
+import councilRoomSrc from './council-room-small.jpg'
+import headspaceSrc from './headspace-small.png'
+import helloWorldSrc from './hello-world-small.jpg'
+import karizmoSrc from './karizmo-small.jpg'
+import keeneySrc from './keeney-small.jpg'
+import nostalgiaSrc from './nostalgia-small.jpg'
+import notFoundSrc from './not-found-small.jpg'
+import pillarsOfCreationSrc from './pillars-of-creation-small.png'
+import pixelRoomSrc from './pixel-room-small.png'
+import prepboardBlackSrc from './prepboard-black-small.jpg'
+import prepboardWhiteSrc from './prepboard-white-small.jpg'
+import scienceCarnivalSrc from './science-carnival-small.jpg'
+import splitSrc from './split-small.jpg'
+import tauSrc from './tau-small.jpg'
+import thisConnectedSrc from './this-connected-small.jpg'
+import wingedVictorySrc from './winged-victory-small.png'
+import wiredSrc from './wired-small.jpg'
+
+export default {
+  architectureSrc,
+  councilRoomSrc,
+  headspaceSrc,
+  helloWorldSrc,
+  karizmoSrc,
+  keeneySrc,
+  nostalgiaSrc,
+  notFoundSrc,
+  pillarsOfCreationSrc,
+  pixelRoomSrc,
+  prepboardBlackSrc,
+  prepboardWhiteSrc,
+  scienceCarnivalSrc,
+  splitSrc,
+  tauSrc,
+  thisConnectedSrc,
+  wingedVictorySrc,
+  wiredSrc,
+}
