977ec034c5fe27ea2e673cae0352436b4a834580
Zack Cheng <zack_cheng@brown.edu>
2022-01-17 11:30:02 -0500

    uploaded image assets for coordinator and others

---
+++ src/img/projects/coordinator/index.ts
@@ -0,0 +1,12 @@
+import image1Src from './coordinator-image-1-medium.png'
+import image2Src from './coordinator-image-2-small.png'
+import image3Src from './coordinator-image-3-small.png'
+import recordingWebmSrc from './coordinator-recording-small.webm'
+import recordingMp4Src from './coordinator-recording-small.mp4'
+
+export default {
+  image1: { aspect: { x: 1196, y: 854 }, src: image1Src },
+  image2: { aspect: { x: 382, y: 273 }, src: image2Src },
+  image3: { aspect: { x: 382, y: 273 }, src: image3Src },
+  recording: { aspect: { x: 764, y: 546 }, webmSrc: recordingWebmSrc, mp4Src: recordingMp4Src },
+}
---
+++ src/img/projects/other/index.ts
@@ -0,0 +1,10 @@
+import raytracerSrc from './raytracer-image-small.png'
+import gmcImageSrc from './gmc-image-small.png'
+import gmcRecordingWebmSrc from './gmc-recording-small.webm'
+import gmcRecordingMp4Src from './gmc-recording-small.mp4'
+
+export default {
+  raytracerImage: { aspect: { x: 586, y: 586 }, src: raytracerSrc },
+  gmcImage: { aspect: { x: 382, y: 382 }, src: gmcImageSrc },
+  gmcRecording: { aspect: { x: 382, y: 382 }, webmSrc: gmcRecordingWebmSrc, mp4Src: gmcRecordingMp4Src },
+}
